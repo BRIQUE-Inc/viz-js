bb0f179ca64ae7af8728c83140b969f0780bc61a
daengdaengLee <gunho1020@gmail.com>
2018-11-26 22:42:21 +0900

    CLN: Customize settings of project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  html, body, #root {
+    height: 100%;
+  }
+`;
+
+ReactDOM.render(
+  <Fragment>
+    <GlobalStyle />
+    <div>Hello World</div>
+  </Fragment>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
