d86452c751a56a09ffac674336fc2e878ab1a175
daengdaengLee <gunho1020@gmail.com>
2018-11-28 14:03:10 +0900

    CLN: Wrap root component using App comp

--- src/index.js
+++ src/index.js
@@ -13,13 +13,14 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-ReactDOM.render(
+const App = () => (
   <Fragment>
     <GlobalStyle />
     <MainPage />
-  </Fragment>,
-  document.getElementById('root'),
+  </Fragment>
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
